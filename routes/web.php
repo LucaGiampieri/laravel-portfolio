d37109c11d7942f03a08a942e18efd67ed87ce8f
Luca Giampieri <luca.giampieri95@gmail.com>
2026-05-06 19:07:57 +0200

    Settaggio Controller, Migration, Seed. Aggiunta resource e autenticatore admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,5 +27,7 @@ Route::middleware(['auth', 'verified'])
             ->name('dashboard');
     });
 
+Route::resource('posts', ProjectController::class)
+    ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
